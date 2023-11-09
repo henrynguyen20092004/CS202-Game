2abfd2162a4c3227c0da4cf2513b827c8527b1f7
OHonSam <ohsam22@apcs.fitus.edu.vn>
2023-11-09 22:33:20 +0700

    Update the Obstacle class

--- src/Obstacle/Obstacle.cpp
+++ src/Obstacle/Obstacle.cpp
@@ -1,5 +1,6 @@
 #include "Obstacle.hpp"
-Obstacle::Obstacle(float mX, float mY, float mWidth, float mHeight): mX(mX), mY(mY), mWidth(mWidth), mHeight(mHeight) {}
+Obstacle::Obstacle(float mX, float mY, float mWidth, float mHeight)
+    : mX(mX), mY(mY), mWidth(mWidth), mHeight(mHeight) {}
 Obstacle::~Obstacle() {}
 void Obstacle::draw(sf::RenderWindow& window) const {
     sf::RectangleShape rectangle(sf::Vector2f(mWidth, mHeight));
@@ -7,3 +8,14 @@ void Obstacle::draw(sf::RenderWindow& window) const {
     rectangle.setFillColor(sf::Color::Red);
     window.draw(rectangle);
 }
+bool Obstacle::isColliding(const sf::FloatRect& other) const {
+        return sf::FloatRect(mX, mY, mWidth, mHeight).intersects(other);
+}
+float Obstacle::getX() const { return mX; }
+float Obstacle::getY() const { return mY; }
+float Obstacle::getWidth() const { return mWidth; }
+float Obstacle::getHeight() const { return mHeight; }
+void Obstacle::setX(float mX) { this->mX = mX; }
+void Obstacle::setY(float mY) { this->mY = mY; }
+void Obstacle::setWidth(float mWidth) { this->mWidth = mWidth; }
+void Obstacle::setHeight(float mHeight) { this->mHeight = mHeight; }
--- src/Obstacle/Obstacle.hpp
+++ src/Obstacle/Obstacle.hpp
@@ -1,25 +1,46 @@
 #ifndef OBSTACLE_HPP
 #define OBSTACLE_HPP
-#include <SFML/Graphics.hpp>
-class Obstacle{//for SFML
-    protected:
-        float mX;
-        float mY;
-        float mWidth;
-        float mHeight;
 
-    public:
-        Obstacle(float mX, float mY, float mWidth, float mHeight);
-        ~Obstacle();
-        // int getX();
-        // int getY();
-        // int getWidth();
-        // int getHeight();
-        // void setX(int x);
-        // void setY(int y);
-        // void setWidth(int width);
-        // void setHeight(int height);
-        void draw(sf::RenderWindow& window) const;
+#include <SFML/Graphics.hpp>
+
+class Obstacle : public sf::Drawable, public sf::Transformable {
+   protected:
+    float mX;
+    float mY;
+    float mWidth;
+    float mHeight;
+
+   public:
+    Obstacle(float x, float y, float width, float height);
+    ~Obstacle();
+
+    // Accessors
+    float getX() const;       // Get X-coordinate
+    float getY() const;       // Get Y-coordinate
+    float getWidth() const;   // Get width
+    float getHeight() const;  // Get height
+
+    // Mutators
+    void setX(float x);            // Set X-coordinate
+    void setY(float y);            // Set Y-coordinate
+    void setWidth(float width);    // Set width
+    void setHeight(float height);  // Set height
+
+    // Draw the obstacle on an SFML window
+    void draw(sf::RenderWindow& window) const;
+
+    // Check if the player is colliding with the obstacle
+    bool isColliding(const sf::FloatRect& other) const;
+
+    // How to random obstacles in tile map
+    // void randomObstacles();
+    // void randomObstacles(int numObstacles);
+
+    // How to create obstacles in tile map
+    // void createObstacles();
+    // void createObstacles(int numObstacles);
+
 
 };
-#endif // !OBSTACLE_HPP
+
+#endif  // !OBSTACLE_HPP

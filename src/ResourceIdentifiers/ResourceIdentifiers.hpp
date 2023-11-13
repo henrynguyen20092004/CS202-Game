d4b8b0dc57c07f04502c71a2cf97c68604338af6
Đinh Cao Minh Quân <minhquandinhcao@gmail.com>
2023-11-13 17:08:45 +0700

    Add more resources

--- src/ResourceIdentifiers/ResourceIdentifiers.hpp
+++ src/ResourceIdentifiers/ResourceIdentifiers.hpp
@@ -1,10 +1,28 @@
 #ifndef RESOURCE_IDENTIFIERS_HPP
 #define RESOURCE_IDENTIFIERS_HPP
 
+namespace States {
+enum class ID {
+    None,
+};
+}
+
 namespace Textures {
 enum class ID {
     Car,
     Truck,
+};
+}
+
+namespace Fonts {
+enum class ID {
+    Main,
+};
+}
+
+namespace Music {
+enum class ID {
+    None,
 };
 }
 

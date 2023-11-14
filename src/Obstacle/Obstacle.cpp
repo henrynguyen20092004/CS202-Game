4ae879323b6f01f2d1da382f9f24400aa4e5f149
OHonSam <ohsam22@apcs.fitus.edu.vn>
2023-11-14 15:50:57 +0700

    Rearrange Obstacle class

--- src/Obstacle/Obstacle.cpp
+++ src/Obstacle/Obstacle.cpp
@@ -1,31 +1,19 @@
 #include "Obstacle.hpp"
-Obstacle::Obstacle(
-    sf::Vector2f position, float width, float height,
-    const std::string& texturePath
-)
-    : mPosition(position), mWidth(width), mHeight(height) {
-    // Load texture
-    sf::Texture texture;
-    if (!texture.loadFromFile(texturePath)) {
-        throw std::runtime_error("LoadFromFile failed");
-    }
+
+sf::Vector2f Obstacle::getSize() const { return mSize; }
+
+void Obstacle::setSize(const sf::Vector2f& size) { mSize = size; }
+
+void Obstacle::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    states.transform *= getTransform();
 }
+
+bool Obstacle::isColliding(const sf::FloatRect& otherHitBox) const {
+    sf::FloatRect hitBox(getPosition(), mSize);
+    return hitBox.intersects(otherHitBox);
+}
+
+Obstacle::Obstacle(const sf::Vector2f& size)
+    : mSize(size) {}
+
 Obstacle::~Obstacle() {}
-void Obstacle::draw(sf::RenderWindow& window) const {
-    sf::RectangleShape obstacle(sf::Vector2f(mWidth, mHeight));
-    obstacle.setPosition(mPosition);
-    obstacle.setFillColor(sf::Color::Red);
-    window.draw(obstacle);
-}
-bool Obstacle::isColliding(const sf::FloatRect& other) const {
-    sf::FloatRect obstacle(mPosition, sf::Vector2f(mWidth, mHeight));
-    return obstacle.intersects(other);
-}
-float Obstacle::getX() const { return mPosition.x; }
-float Obstacle::getY() const { return mPosition.y; }
-float Obstacle::getWidth() const { return mWidth; }
-float Obstacle::getHeight() const { return mHeight; }
-void Obstacle::setX(float mX) { mPosition.x = mX; }
-void Obstacle::setY(float mY) { mPosition.y = mY; }
-void Obstacle::setWidth(float mWidth) { this->mWidth = mWidth; }
-void Obstacle::setHeight(float mHeight) { this->mHeight = mHeight; }
--- src/Obstacle/Obstacle.hpp
+++ src/Obstacle/Obstacle.hpp
@@ -1,50 +1,32 @@
 #ifndef OBSTACLE_HPP
 #define OBSTACLE_HPP
 
-#include <SFML/Graphics.hpp>
+#include "../ResourceHolder/ResourceHolder.hpp"
+
+/*
+    TODO: create obstacle in tile map with random/given position
+*/
 
 class Obstacle : public sf::Drawable, public sf::Transformable {
-   protected:
-    sf::Vector2f mPosition;
-    float mWidth;
-    float mHeight;
-
    public:
-    // Constructor
-    Obstacle(sf::Vector2f position, float width, float height, const std::string& texturePath);
-    // Destructor
+    typedef std::unique_ptr<Obstacle> Ptr;
+
+    sf::Vector2f getSize() const;
+
+    void setSize(const sf::Vector2f& size);
+
+    virtual void update(float deltaTime) = 0;
+
+    bool isColliding(const sf::FloatRect& otherHitBox) const;
+
+   protected:
+    Obstacle(const sf::Vector2f& size);
     ~Obstacle();
 
-    // Accessors
-    float getX() const;       // Get X-coordinate
-    float getY() const;       // Get Y-coordinate
-    float getWidth() const;   // Get width
-    float getHeight() const;  // Get height
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
-    // Mutators
-    void setX(float x);            // Set X-coordinate
-    void setY(float y);            // Set Y-coordinate
-    void setWidth(float width);    // Set width
-    void setHeight(float height);  // Set height
-
-    // Draw the obstacle on an SFML window
-    void draw(sf::RenderWindow& window) const;
-
-    // Check if the player is colliding with the obstacle
-    bool isColliding(const sf::FloatRect& other) const;
-
-    // How to random obstacles in tile map
-    // void randomObstacles();
-    // void randomObstacles(int numObstacles);
-
-    // How to create obstacles in tile map
-    // void createObstacles();
-    // void createObstacles(int numObstacles);
-
-    // How to create obstacles in tile map with a given position
-    // void createObstacles(sf::Vector2f position);
-    // void createObstacles(sf::Vector2f position, int numObstacles);
-    
+   private:
+    sf::Vector2f mSize;
 };
 
-#endif  // !OBSTACLE_HPP
+#endif

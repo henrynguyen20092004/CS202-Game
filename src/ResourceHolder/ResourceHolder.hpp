51b74c7e4b156daa3c769f3af8406843821bd5d6
Đinh Cao Minh Quân <minhquandinhcao@gmail.com>
2023-11-21 21:49:03 +0700

    Change extension for ResourceHolder: from  .tpp to .inl

--- src/ResourceHolder/ResourceHolder.hpp
+++ src/ResourceHolder/ResourceHolder.hpp
@@ -23,6 +23,8 @@ class ResourceHolder {
 typedef ResourceHolder<sf::Texture, Textures::ID> TextureHolder;
 typedef ResourceHolder<sf::Font, Fonts::ID> FontHolder;
 
-#include "ResourceHolder.tpp"
+#include "ResourceHolder.inl"
+
+using TextureHolder = ResourceHolder<sf::Texture, Textures::ID>;
 
 #endif  // RESOURCE_HOLDER_HPP

43c518dd530e649f9f41282d4ebfcf62a2daa57b
Đào Thanh Hải <126577662+imbigbob@users.noreply.github.com>
2023-11-23 08:42:42 +0700

    set audio

--- src/Animal/Animal.hpp
+++ src/Animal/Animal.hpp
@@ -1,6 +1,8 @@
 #ifndef ANIMAL_HPP
 #define ANIMAL_HPP
 
+#include <SFML/Audio.hpp>
+
 #include "../MovableSpriteNode/MovableSpriteNode.hpp"
 
 class Animal : public MovableSpriteNode {
@@ -8,9 +10,11 @@ class Animal : public MovableSpriteNode {
     typedef std::unique_ptr<Animal> Ptr;
 
    protected:
+    sf::SoundBuffer mSoundBuffer;
+    sf::Sound mSound;
     Animal(const sf::Texture& texture);
-
     virtual void updateCurrent(float deltaTime) = 0;
+    // virtual void powerUp() = 0;
 };
 
 #endif

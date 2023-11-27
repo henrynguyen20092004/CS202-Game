e1d779c5833cc2d3bc91ccf2f5336cd0fc3789cc
Nguyễn Vinh Hiển <71540470+henrynguyen20092004@users.noreply.github.com>
2023-11-27 16:49:16 +0700

    Remove audio

--- src/Animal/Animal.hpp
+++ src/Animal/Animal.hpp
@@ -1,8 +1,6 @@
 #ifndef ANIMAL_HPP
 #define ANIMAL_HPP
 
-#include <SFML/Audio.hpp>
-
 #include "../Identifier/Identifier.hpp"
 #include "../MovableSpriteNode/MovableSpriteNode.hpp"
 
@@ -11,16 +9,9 @@ class Animal : public MovableSpriteNode {
     typedef std::unique_ptr<Animal> Ptr;
 
    protected:
-    sf::SoundBuffer mSoundBuffer;
-    sf::Sound mSound;
-
     Animal(const sf::Texture& texture);
     virtual void updateCurrent(float deltaTime) = 0;
     // virtual void powerUp() = 0;
-
-   private:
-    virtual sf::Sound getSound() = 0;
-    virtual void setSound(Sound::ID soundBuffer) = 0;
 };
 
 #endif
